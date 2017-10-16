e18e3efdbb681a810674421ac4a7e21ca67ee67f
Brandon Morris <bmorris5@stu.jsu.edu>
2017-10-16 11:39:25 -0500

    Merge branch 'master' of https://github.com/Astella8/CS310DB
    # Conflicts:
    #	.gitignore
    #	src/database.java

--- src/edu/jsu/mcis/Punch.java
+++ src/edu/jsu/mcis/Punch.java
@@ -4,77 +4,77 @@ import java.util.*;
 import java.text.SimpleDateFormat;
 
 public class Punch {
-	
-	private int shiftId;
-        private String punchId;
-	private String badgeId;
-        private String punchDescription;
-	private int terminalId;
-	private GregorianCalendar original;
-	private GregorianCalendar adjusted;
-        private String sdf;
-	
-		public Punch(int terminalId, String badgeId, int shiftId, long originalts, long adjustedts){
-			original = new GregorianCalendar();
-			adjusted = new GregorianCalendar();
-                        originalts = (originalts * 1000);
-                        adjustedts = (originalts * 1000);
-                        original.setTimeInMillis(originalts);
-                    adjusted.setTimeInMillis(adjustedts);
-			this.terminalId = terminalId;
-			this.badgeId = badgeId;
-			this.shiftId = shiftId;   
-                        //String testdate =  format.format(adjusted.getTime());
-                        sdf = new SimpleDateFormat("EEE MM/dd/YYYY HH:mm:ss").format(original.getTime()).toUpperCase();
-                        
 
-                        
-                        
-		}
-		
+    private int shiftId;
+    private String punchId;
+    private String badgeId;
+    private String punchDescription;
+    private int terminalId;
+    private GregorianCalendar original;
+    private GregorianCalendar adjusted;
+    private String sdf;
 
-   public String getPunchDescription(){
-       return punchDescription;
-   }
-   public void setDescription(String punchDescription){
-       this.punchDescription = punchDescription;
-   } 
-   public String getPunchId(){
-       return punchId;
-   }
-   public void setPunchId(String punchId){
-       this.punchId = punchId;
-       
-   }
-   public String getBadgeId(){
-       return badgeId;
-   }
-   public int getTerminalId(){
-       return terminalId;
-   }
-   public int getShiftId(){
-       return shiftId;
-   }
-   public GregorianCalendar getOriginalTimeStamp(){
-       return adjusted;
-   }
-    public GregorianCalendar getAdjustedTimeStamp(){
-       return adjusted;
-   }
-   
-   public String printOriginalTimestamp(){
-       String Status = "";
-       if(shiftId == 1){
-           Status = " CLOCKED IN: ";
-       }
-       else if(shiftId == 0){
-           Status = " CLOCKED OUT: ";
-       }
-       else{
-           Status = " TIMED OUT: ";
-       }
-       return "#" + badgeId + Status + sdf;
-   }
-   
-	
+    public Punch(int terminalId, String badgeId, int shiftId, long originalts, long adjustedts) {
+        original = new GregorianCalendar();
+        adjusted = new GregorianCalendar();
+        originalts = (originalts * 1000);
+        adjustedts = (originalts * 1000);
+        original.setTimeInMillis(originalts);
+        adjusted.setTimeInMillis(adjustedts);
+        this.terminalId = terminalId;
+        this.badgeId = badgeId;
+        this.shiftId = shiftId;
+        //String testdate =  format.format(adjusted.getTime());
+        sdf = new SimpleDateFormat("EEE MM/dd/YYYY HH:mm:ss").format(original.getTime()).toUpperCase();
+    }
+
+    public String getPunchDescription() {
+        return punchDescription;
+    }
+
+    public void setDescription(String punchDescription) {
+        this.punchDescription = punchDescription;
+    }
+
+    public String getPunchId() {
+        return punchId;
+    }
+
+    public void setPunchId(String punchId) {
+        this.punchId = punchId;
+
+    }
+
+    public String getBadgeId() {
+        return badgeId;
+    }
+
+    public int getTerminalId() {
+        return terminalId;
+    }
+
+    public int getShiftId() {
+        return shiftId;
+    }
+
+    public GregorianCalendar getOriginalTimeStamp() {
+        return adjusted;
+    }
+
+    public GregorianCalendar getAdjustedTimeStamp() {
+        return adjusted;
+    }
+
+    public String printOriginalTimestamp() {
+        String Status = "";
+        if (shiftId == 1) {
+            Status = " CLOCKED IN: ";
+        } else if (shiftId == 0) {
+            Status = " CLOCKED OUT: ";
+        } else {
+            Status = " TIMED OUT: ";
+        }
+        return "#" + badgeId + Status + sdf;
+    }
+
 }
